56c6db21aa823346764118d341214f759be118e9
anurag-chandra9 <anuragkrstm01@gmail.com>
2024-10-30 23:18:31 +0530

    first

--- myCart/shop/views.py
+++ myCart/shop/views.py
@@ -4,10 +4,21 @@ from .models import Product
 from math import ceil
 # Create your views here.
 def index(request):
-    products= Product.objects.all()
-    n= len(products)
-    nSlides= n//4 + ceil((n/4) + (n//4))
-    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    # products= Product.objects.all()
+    # n= len(products)
+    # nSlides= n//4 + ceil((n/4) + (n//4))
+   #params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    # allprods=[[products,range(1,nSlides),nSlides],
+    #           [products,range(1,nSlides),nSlides]]
+    allprods=[]
+    catprods=Product.objects.values('category','id')
+    cats={item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n=len(prod)
+        nSlides=n // 4 + ceil((n/4)-(n//4))
+        allprods.append([prod,range(1, nSlides), nSlides])
+    params={'allprods':allprods}  
     return render(request,"shop/index.html", params)
 def about(request):
     return render(request,'shop/about.html')
