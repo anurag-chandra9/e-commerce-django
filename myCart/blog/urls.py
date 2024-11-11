c539b49a9d146baa98f3e7187b4fb980e902b4be
anurag-chandra9 <anuragkrstm01@gmail.com>
2024-11-11 15:58:28 +0530

    first

--- myCart/blog/urls.py
+++ myCart/blog/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
-from.import views
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
 urlpatterns = [
-    path("",views.index,name="ShopHome")
+    path('', views.index,name="BlogHome"),
+    path('blogpost/', views.blogpost,name="blogPost"),
 ]
